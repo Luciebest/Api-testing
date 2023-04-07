c27a0039a3005d1c138ec625f3aa10de28cb663b
Robin_Budulea <robin.budulea@gmail.com>
2023-04-07 23:59:43 +0300

    Tema_16_Robin_Budulea

---
+++ Robin-Budulea/src/test/java/com/cegeka/academy/qa/backpack/TestBackpack.java
@@ -0,0 +1,43 @@
+package com.cegeka.academy.qa.backpack;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBackpack {
+    private String email;
+    private String password;
+    private String registerToken;
+    private String loginToken;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRegisterToken() {
+        return registerToken;
+    }
+
+    public void setRegisterToken(String registerToken) {
+        this.registerToken = registerToken;
+    }
+
+    public String getLoginToken() {
+        return loginToken;
+    }
+
+    public void setLoginToken(String loginToken) {
+        this.loginToken = loginToken;
+    }
+}
